76bc868bd54681874f62f255871624cf86a91e2f
Chandrapal Singh Chouhan <74819484+Chandrapalsingh12@users.noreply.github.com>
2023-04-09 10:41:02 +0530

    first commit

--- src/page/Homepage.jsx
+++ src/page/Homepage.jsx
@@ -1,38 +1,42 @@
 import React, { useEffect, useState } from "react";
-import {Link} from "react-router-dom"
+import { Link } from "react-router-dom";
 
 function Homepage() {
-    const [workflows, setWorkflows] = useState([]);
+  const [workflows, setWorkflows] = useState([]);
 
-    useEffect(() => {
-      fetch('https://64307b10d4518cfb0e50e555.mockapi.io/modules')
-        .then(response => response.json())
-        .then(data => setWorkflows(data));
-    }, []);
-  
-    return (
-      <div className="workflow-list">
-        <h1>Workflow List</h1>
-        <table>
-          <thead>
-            <tr>
-              <th>Name</th>
-              <th>InputType</th>
-              <th>CreatedAt</th>
-            </tr>
-          </thead>
-          <tbody>
-            {workflows.map(workflow => (
-              <tr key={workflow.id} onClick={() => window.location.href = `/workflow/${workflow.id}`}>
-                <td>{workflow.name}</td>
-                <td>{workflow.input_type}</td>
-                <td>{workflow.createdAt}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
-    );
+  useEffect(() => {
+    fetch("https://64307b10d4518cfb0e50e555.mockapi.io/modules")
+      .then((response) => response.json())
+      .then((data) => setWorkflows(data));
+  }, []);
+
+  return (
+    <div className="workflow-list">
+      <h1>Workflow List</h1>
+      <table>
+    <thead>
+      <tr>
+        <th>Name</th>
+        <th>InputType</th>
+        <th>CreatedAt</th>
+      </tr>
+    </thead>
+    <tbody>
+      {workflows.map((workflow) => (
+        <tr key={workflow.id}>
+          <td>
+            <Link to={`/workflow/${workflow.id}`}>
+              {workflow.name}
+            </Link>
+          </td>
+          <td>{workflow.input_type}</td>
+          <td>{workflow.createdAt}</td>
+        </tr>
+      ))}
+    </tbody>
+  </table>
+    </div>
+  );
 }
 
 export default Homepage;
--- src/page/Workflows.jsx
+++ src/page/Workflows.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-// import ReactFlow, { Controls, Background } from "reactflow";
 import ReactFlow, { Controls, Background, addEdge } from "reactflow";
 
 import "reactflow/dist/style.css";
